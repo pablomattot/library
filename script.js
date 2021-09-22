2a8c4d9114f64025e657389af3a9fa10f79f8d3b
Pablo Mattot <mattotpablo@gmail.com>
2021-09-22 10:31:08 +0200

    Added button to HTML and eventlistener to make it run the getDetails and the addBookToLibrary functions

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
 let library = [];
 
-let titleInput = document.querySelector("#title").value;
-
-function Book(name, author, pages) {
-    this.name = name
+function Book(title, author, pages) {
+    this.title = title
     this.author = author
     this.pages = pages
 }
 
-function addBookToLibrary(name, author, pages) {
-    library.push(new Book(name, author, pages));
+function addBookToLibrary(title, author, pages) {
+    library.push(new Book(title, author, pages));
 }
 
-
 function getDetails() {
-    let name = prompt("Book title?");
-    let author = prompt("Who is the author?");
-    let pages = prompt("Total pages?");
+    let title = document.querySelector("#title").value;
+    let author = document.querySelector("#author").value;
+    let pages = document.querySelector("#pages").value;
 
-    return [name, author, pages];
+    return [title, author, pages];
     
 }
 
-// let details = getDetails();
-
-// addBookToLibrary(details[0], details[1], details[2]);
+const btn = document.querySelector("button");
+btn.addEventListener('click', () => {
+    let details = getDetails();
+    addBookToLibrary(details[0], details[1], details[2]);
+})
 
 function displayBooks() {
     console.table(library);
