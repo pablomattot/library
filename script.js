c771274404b89a0a143c92c74fc48189558ac7b3
Pablo Mattot <mattotpablo@gmail.com>
2021-09-22 11:55:02 +0200

    Created function to display the books and styled the book card layout

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-let library = [];
+let library = [
+    {
+        cover: "https://images-na.ssl-images-amazon.com/images/I/91b0C2YNSrL.jpg",
+        title: "The Hobbit",
+        author: "Tolkien",
+        pages: 295
+    },
+    {
+        cover: "https://images-na.ssl-images-amazon.com/images/I/51MFw+si6RL._SX324_BO1,204,203,200_.jpg",
+        title: "The Book of Longings",
+        author: "Sue Monk",
+        pages: 228
+    }
+];
 
-function Book(title, author, pages) {
+function Book(cover, title, author, pages, read) {
+    this.cover = cover
     this.title = title
     this.author = author
     this.pages = pages
+    this.read = read
 }
 
 function addBookToLibrary(title, author, pages) {
@@ -16,7 +31,7 @@ function getDetails() {
     let pages = document.querySelector("#pages").value;
 
     return [title, author, pages];
-    
+
 }
 
 const btn = document.querySelector("button");
@@ -25,6 +40,29 @@ btn.addEventListener('click', () => {
     addBookToLibrary(details[0], details[1], details[2]);
 })
 
+
+const bookList = document.querySelector(".book-list");
 function displayBooks() {
-    console.table(library);
+    library.forEach(book => {
+        bookList.innerHTML += `
+        <div class="book-card"">
+            <div class="overflow-wrapper">
+                <div class="card-background" style="background-image:url(${book.cover})">
+            </div>
+            </div>
+            <div class="book-img"><img src="${book.cover}" alt=""></div>
+            <div class="book-details">
+                <p class="book-title">${book.title}</p>
+                <ul>
+                    <li>${book.author}</li>
+                    <li>&#8226;</li>
+                    <li>${book.pages} pages</li>
+                    <li>&#8226;</li>
+                    <li>${book.read}</li>
+                </ul>
+            </div>
+        </div>`;
+    })
 }
+
+displayBooks();
